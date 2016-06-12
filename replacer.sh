66d3aac1a246c202d14d11870c44f6619a4e0048
Taras Dolynyuk <trsdln@gmail.com>
2016-06-12 21:19:52 +0300

    Clean up code, add readme, license

--- replacer.sh
+++ replacer.sh
@@ -2,22 +2,22 @@
 
 echo "Started"
 
-IMP='import { Meteor }'
-FIND='Meteor'
-REPLACE='CoolModule'
-FL=main1.js
+TARGET_FILE=$1
+FIND=$2
+REPLACE=$3
+TOP_LINE=$4
+TEMP_FILE=.tmp
 
-cp todo.txt $FL
-
-if grep -q "${FIND}" $FL; then # file contains string we need to replace
-    # check if we need to add import
-    if grep -q "${IMP}" "${FL}"; then
-        echo "Insert import in ${FL}"
-        sed -e 'a\'$'\n'"${IMP}" $FL > tmp && mv -f tmp $FL && echo "YES" || echo "OOPS"
+if grep -q "${FIND}" ${TARGET_FILE}; then # file contains string we need to replace
+    # check if we need to top line
+    if grep -q "${TOP_LINE}" "${TARGET_FILE}"; then
+        echo "Insert top line in ${TARGET_FILE}"
+        sed -e 'a\'$'\n'"${TOP_LINE}" ${TARGET_FILE} > ${TEMP_FILE} && mv -f ${TEMP_FILE} ${TARGET_FILE}
     fi
 
     # replace old usages with new one
-    sed 's/${FIND}/${REPLACE}/g' $FL > tmp && mv -f tmp $FL
     echo "Replace module usages"
+    sed 's/${FIND}/${REPLACE}/g' ${TARGET_FILE} > ${TEMP_FILE} && mv -f ${TEMP_FILE} ${TARGET_FILE}
 fi
 
+echo "Finished"
